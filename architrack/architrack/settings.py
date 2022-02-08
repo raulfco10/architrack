986e06a3ea0912e8203467143362913681fa0c9a
Raul Orozco <Raul.Orozco@cantire.com>
2022-02-08 14:29:53 -0600

    Cambiando settings del proyecto

--- architrack/architrack/settings.py
+++ architrack/architrack/settings.py
@@ -126,22 +126,22 @@ WSGI_APPLICATION = 'architrack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': os.environ.get('DB_NAME'),
-#        'USER': os.environ.get('DB_USER'),
-#        'PASSWORD':os.environ.get('DB_PASS'),
-#        'HOST': os.environ.get('DB_HOST'),
-#        'PORT': '5432',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
